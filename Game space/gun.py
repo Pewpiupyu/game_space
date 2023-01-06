ae555eb7b236857ea5e8798a781864c7a3d44551
Maksosss <popov.sneg@yandex.ru>
2023-01-06 23:46:58 +0300

    ok

--- Game space/gun.py
+++ Game space/gun.py
@@ -1,17 +1,17 @@
 import pygame
 
-
-class Gun():
+class Gun(Sprite):
 
     def __init__(self, screen):
-        """инициализаация пушки"""
+        """инициализация пушки"""
+
         self.screen = screen
-        self.image = pygame.image.load('image/pixil-frame-0.png')
+        self.image = pygame.image.load('images/pixil-frame-0 (1).png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.botton = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom
 
     def output(self):
         """рисование пушки"""
-        self.screen.blit( self.image, self.rect )
+        self.screen.blit(self.image, self.rect)
